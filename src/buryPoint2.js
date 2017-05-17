187c181c546ee6066349666cd929798608c30068
zhangxiang <958033967@qq.com>
2017-05-17 22:52:21 +0800

    add func

--- src/buryPoint2.js
+++ src/buryPoint2.js
@@ -6,55 +6,95 @@
     let resource = perf.timing;
 
     console.log(resource);
+
+    
     /**
      * static source count
      */
     let entries = perf.getEntries();
-    let javascriptNum = 0, cssNum = 0, imgNum = 0;
+    let javascriptNum = 0, cssNum = 0, imgNum = 0, javascriptTime = 0, cssTime = 0, imgTime = 0;
 
-    entries.foeEach(function(data, i){
+    entries.forEach(function(data, i){
 
+        //分类计算静态资源数量
         switch (data.initiatorType) {
             case 'script': 
                 javascriptNum ++;
+                javascriptTime += data.responseEnd - data.startTime
                 break;
             case 'css':
                 cssNum ++;
+                cssTime += data.responseEnd - data.startTime;
                 break;
             case 'img':
                 imgNum ++;
+                imgTime += data.responseEnd - data.startTime;
                 break;
-        }   
-        
-
+        } 
     });
 
     const staticSourceInfo = {
         'num': entries.length,
-        'javascriptNum': 1,
-        'cssNum': 2,
-        'imgNum': 3
+        'javascriptNum': javascriptNum,
+        'cssNum': cssNum,
+        'imgNum': imgNum,
+        'javascriptUseTime': javascriptTime,
+        'cssUseTime': cssTime,
+        'imgUsetime': imgTime
     }
 
-    policeReport.staticSourceInfo = staticSourceInfo;
+    policeReport['静态资源加载情况'] = staticSourceInfo;
+
+
     /**
      * DNS count -- domainlookup time
      */
     let DNSquery = resource.domainLookupEnd - resource.domainLookupStart;
+    policeReport['DNS 查询时间'] = DNSquery;
+
+
+
     /**
      * TCP time count -- connect time
      */
     let TCPconnect = resource.connectEnd - resource.connectStart;
+    policeReport['TCP 连接时间'] = TCPconnect;
+
+
     /**
      * request time 
      */
     // let requestTime = resource.requestStart - resource.responseStart;
+
+
+
     /**
      * response time
      */
     let responseTime = resource.responseEnd - resource.responseStart;
+    policeReport['请求时间'] = responseTime;
+
+
+    /**
+     * 白屏时间
+     */
+    let whiteTime = resource.responseStart - resource.navigationStart;
+    policeReport['白屏时间'] = whiteTime;
+
+
+    /**
+     * 解析 DOM 树耗费时间
+     */
+    let DOMTime = resource.domContentLoadedEventEnd - resource.navigationStart;
+    policeReport['解析 DOM 耗费时间'] = DOMTime;
+
+
     /**
      * load time
      */
+    let loadTime = resource.loadEventEnd - resource.navigationStart;
+    policeReport['onload 加载时间'] = loadTime;
 
+    console.log(policeReport);
+    //分开 IE 与高级浏览器
 })();
