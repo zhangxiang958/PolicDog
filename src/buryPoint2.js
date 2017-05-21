e6b98ae307ad85d8aee4f44f4549dd50eaa92923
zhangxiang <958033967@qq.com>
2017-05-21 23:08:32 +0800

    add func

--- src/buryPoint2.js
+++ src/buryPoint2.js
@@ -1,99 +1,150 @@
+/**
+ * @file: PoliceDog web performance script for client side
+ * @author by zhangxiang
+ * @update 2017.05
+ */
+
 (function(){
     
-    console.log(performance.timing);
-
-    let perf = window.performance;
-    let resource = perf.timing;
-
-    console.log(resource);
-
-    
     /**
-     * static source count
+     * Browser compatibility:
+     * Some brower cannot handle the interface(window.performance), so we must add some code to compatibile some bowser
+     * 
      */
-    let entries = perf.getEntries();
-    let javascriptNum = 0, cssNum = 0, imgNum = 0, javascriptTime = 0, cssTime = 0, imgTime = 0;
 
-    entries.forEach(function(data, i){
 
-        //分类计算静态资源数量
-        switch (data.initiatorType) {
-            case 'script': 
-                javascriptNum ++;
-                javascriptTime += data.responseEnd - data.startTime
-                break;
-            case 'css':
-                cssNum ++;
-                cssTime += data.responseEnd - data.startTime;
-                break;
-            case 'img':
-                imgNum ++;
-                imgTime += data.responseEnd - data.startTime;
-                break;
-        } 
-    });
+    if(window.addEventListener){
 
-    const staticSourceInfo = {
-        'num': entries.length,
-        'javascriptNum': javascriptNum,
-        'cssNum': cssNum,
-        'imgNum': imgNum,
-        'javascriptUseTime': javascriptTime,
-        'cssUseTime': cssTime,
-        'imgUsetime': imgTime
+        window.addEventListener('load', function(){
+
+
+        });
+
+    } else if(window.attachEvent) {
+
+        window.attachEvent('onload', function(){
+
+        });
+        
+    } else {
+
+        if(window.onload) {
+
+            let loadFunc = window.onload;
+
+            window.onload = function(){
+
+
+                /**do some thing */
+
+                loadFunc();
+            }
+
+        } else {
+            window.onload = function(){
+                /**do something */
+            }
+        }
+
     }
 
-    policeReport['静态资源加载情况'] = staticSourceInfo;
+    function getPerformance(){
+        let perf = null;
+        
+        perf = window.performance ? window.performance : (window.wekitPerformance ? window.webkitPerformance : window.msPerformance);
 
+        return perf;
+    }
+    
+    /**
+     * to count the static source situation
+     */
+    function toAnalysisStaticResource(perf){
+
+        let entries = perf.getEntries();
+        let javascriptNum = 0, cssNum = 0, imgNum = 0, javascriptTime = 0, cssTime = 0, imgTime = 0;
+
+        entries.forEach(function(data, i){
+
+            //to count time of the different type of resource
+            switch (data.initiatorType) {
+                case 'script': 
+                    javascriptNum ++;
+                    javascriptTime += data.responseEnd - data.startTime
+                    break;
+                case 'css':
+                    cssNum ++;
+                    cssTime += data.responseEnd - data.startTime;
+                    break;
+                case 'img':
+                    imgNum ++;
+                    imgTime += data.responseEnd - data.startTime;
+                    break;
+            } 
+        });
+
+        const staticSourceInfo = {
+            'num': entries.length,
+            'javascriptNum': javascriptNum,
+            'cssNum': cssNum,
+            'imgNum': imgNum,
+            'javascriptUseTime': javascriptTime,
+            'cssUseTime': cssTime,
+            'imgUsetime': imgTime
+        }
+
+        return staticSourceInfo;
+    }
+
+    policeReport['staticResource loadSituation'] = toAnalysisStaticResource(perf);
 
     /**
-     * DNS count -- domainlookup time
+     * to count the DNS Query time
      */
     let DNSquery = resource.domainLookupEnd - resource.domainLookupStart;
-    policeReport['DNS 查询时间'] = DNSquery;
+    policeReport['DNS Query'] = DNSquery;
 
 
 
     /**
-     * TCP time count -- connect time
+     *to count the TCP connect time
      */
     let TCPconnect = resource.connectEnd - resource.connectStart;
-    policeReport['TCP 连接时间'] = TCPconnect;
+    policeReport['TCP Connect'] = TCPconnect;
 
 
     /**
-     * request time 
+     * to count the request time 
      */
-    // let requestTime = resource.requestStart - resource.responseStart;
-
+    let requestTime = resource.requestEnd - resource.requestStart;
 
 
     /**
-     * response time
+     * to count the response time
      */
     let responseTime = resource.responseEnd - resource.responseStart;
-    policeReport['请求时间'] = responseTime;
+    policeReport['Request Time'] = responseTime;
 
 
     /**
      * 白屏时间
      */
     let whiteTime = resource.responseStart - resource.navigationStart;
-    policeReport['白屏时间'] = whiteTime;
+    policeReport['WhiteScreen Time'] = whiteTime;
 
 
     /**
      * 解析 DOM 树耗费时间
      */
-    let DOMTime = resource.domContentLoadedEventEnd - resource.navigationStart;
-    policeReport['解析 DOM 耗费时间'] = DOMTime;
+    let DOMAnalysis = resource.domContentLoadedEventEnd - resource.navigationStart;
+    policeReport['DOM Analysis'] = DOMAnalysis;
 
 
     /**
      * load time
      */
     let loadTime = resource.loadEventEnd - resource.navigationStart;
-    policeReport['onload 加载时间'] = loadTime;
+    policeReport['Load Time'] = loadTime;
 
     console.log(policeReport);
     //分开 IE 与高级浏览器
